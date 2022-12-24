d44117dff0bf91cc2da79bdd678b4ed26a2afd56
Hao Chen <hche8093@uni.sydney.edu.au>
2022-12-24 11:04:40 +1100

    Initial monster-react-app

---
+++ monster-rolodex/src/App.js
@@ -0,0 +1,34 @@
+import { Component } from "react";
+
+import "./App.css";
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			monsters: [
+				{
+					name: "Linda",
+				},
+				{
+					name: "Frank",
+				},
+				{
+					name: "Jacky",
+				},
+			],
+		};
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{this.state.monsters.map((monster) => {
+					return <h1 key={monster.name}>{monster.name}</h1>;
+				})}
+			</div>
+		);
+	}
+}
+
+export default App;
