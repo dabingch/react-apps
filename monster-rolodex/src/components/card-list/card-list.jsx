9a6d860c6ace5c4df67350a2ae63b8987c99af0f
Hao Chen <hche8093@uni.sydney.edu.au>
2022-12-26 17:48:45 +1100

    Switch to functional component completely

--- monster-rolodex/src/components/card-list/card-list.jsx
+++ monster-rolodex/src/components/card-list/card-list.jsx
@@ -1,21 +1,13 @@
-import { Component } from "react";
-
 import "./card-list.styles.css";
 
 import Card from "../card/card";
 
-class CardList extends Component {
-	render() {
-		const { monster } = this.props;
-
-		return (
-			<div className="card-list">
-				{monster.map((monster) => {
-					return <Card monster={monster} />;
-				})}
-			</div>
-		);
-	}
-}
+const CardList = ({ monster }) => (
+	<div className="card-list">
+		{monster.map((monster) => {
+			return <Card monster={monster} key={monster.id} />;
+		})}
+	</div>
+);
 
 export default CardList;
