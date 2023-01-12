6b0948ce392b92d178a8d81d1d54bdf48c01b82f
Hao Chen <hche8093@uni.sydney.edu.au>
2023-01-12 11:21:59 +1100

    Add ts in card and card list component

--- monster-rolodex/src/App.tsx
+++ monster-rolodex/src/App.tsx
@@ -1,19 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, ChangeEvent } from "react";
+
+import { getData } from "./utils/data.utils";
 
 import "./App.css";
 
 import CardList from "./components/card-list/card-list";
 import SearchBox from "./components/search-box/search-box";
 
+export type Monster = {
+	id: string;
+	name: string;
+	email: string;
+}
+
 const App = () => {
 	const [searchField, setSearchField] = useState("");
-	const [monsters, setMonsters] = useState([]);
+	const [monsters, setMonsters] = useState<Monster[]>([]);
 	const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
 	useEffect(() => {
-		fetch("https://jsonplaceholder.typicode.com/users")
-			.then((response) => response.json())
-			.then((users) => setMonsters(users));
+
+		const fetchUsers = async () => {
+			const users = await getData<Monster[]>("https://jsonplaceholder.typicode.com/users");
+			setMonsters(users);
+		}
+
+		fetchUsers()
 	}, []);
 
 	useEffect(() => {
@@ -24,7 +36,7 @@ const App = () => {
 		setFilteredMonsters(newFilteredMonsters);
 	}, [searchField, monsters]);
 
-	const onSearchChange = (e) => {
+	const onSearchChange = (e: ChangeEvent<HTMLInputElement>): void => {
 		const searchString = e.target.value.toLowerCase();
 
 		setSearchField(searchString);
--- monster-rolodex/src/components/card-list/card-list.tsx
+++ monster-rolodex/src/components/card-list/card-list.tsx
@@ -1,8 +1,13 @@
 import "./card-list.styles.css";
 
 import Card from "../card/card";
+import { Monster } from "../../App";
 
-const CardList = ({ monster }) => (
+type CardListProps = {
+	monster: Monster[];
+}
+
+const CardList = ({ monster }: CardListProps) => (
 	<div className="card-list">
 		{monster.map((monster) => {
 			return <Card monster={monster} key={monster.id} />;
